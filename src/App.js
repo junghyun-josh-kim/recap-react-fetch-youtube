a521313710b63b09c162ee51bcfd7e837adfb1ff
Junghyun Josh Kim <111531443+junghyun-josh-kim@users.noreply.github.com>
2023-08-07 20:21:09 -0400

    Youtube API + fetch data + useContext
    1. Youtube API to fetch data
    
    2. axios + useContext + custom hook
    
    3. display 'search' and 'most popular'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { YoutubeApiProvider } from './context/YoutubeApiContext';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
+import Youtube, { search } from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
-  return <div>Videos {keyword ? `${keyword}` : 'Hot Trends'}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `${keyword}` : 'Hot Trends'}</div>
+      {isLoading && <p>Loading</p>}
+      {error && <p>Error</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard
+              key={video.id}
+              video={video}
+            />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
